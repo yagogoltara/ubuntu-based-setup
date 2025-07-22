4a2ab265b5b24930fac100a0cf8b88612e915d2b
Yago Goltara <77808082+yagogoltara@users.noreply.github.com>
2025-07-21 23:26:02 -0300

    new version

--- setup.sh
+++ setup.sh
@@ -3,12 +3,9 @@
 ### UPDATE PACKAGES
 apt update && apt upgrade -y
 
-### INSTALL GNOME TOOLS
-apt install -y gnome-tweak-tools
-
 ### INSTALL DOCKER
 apt-get update
-apt-get install ca-certificates curl
+apt-get install -y ca-certificates curl
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
@@ -17,6 +14,7 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 usermod -aG docker $USER
 systemctl enable docker.service
 systemctl enable containerd.service
@@ -42,3 +40,19 @@ apt install -y software-properties-common apt-transport-https wget
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add -
 add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 apt install code -y
+
+### INSTALL AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+### INSTALL KUBECTL
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+### INSTALL ANYDESK
+wget https://download.anydesk.com/linux/anydesk_7.0.1-1_amd64.deb
+dpkg -i anydesk*.deb
+
+### INSTALL TILIX 
+apt install -y tilix
